9859c9718c8d997d461c3c03524837cf69d6a453
Vu Minh Hieu <vuminhhieu1311@gmail.com>
2022-10-03 23:17:07 +0700

    feat: logout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+var logoutBtn = document.querySelector('#logout-btn');
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        document.getElementById('logout-form').submit();
+    });
+}
